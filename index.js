7c2492d602cbc32e5f214ccb909a619362e1f993
Rafael Cardenas <66276428+ra-car@users.noreply.github.com>
2020-10-04 22:12:48 -0500

    Token methods
    Implementation of token's post method

--- index.js
+++ index.js
@@ -96,5 +96,6 @@ let unifiedServer = function(req,res){
 // Define a request router
 var router = {
   'ping': handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'token' : handlers.token
 };
