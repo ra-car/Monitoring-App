42a4f428bea3458c3a91985c9eedae8681e957e9
Rafael Cardenas <66276428+ra-car@users.noreply.github.com>
2020-10-14 22:12:31 -0500

    Checks post method
    Implemneted the checks post method

---
+++ config.js
@@ -0,0 +1,32 @@
+/*
+* Create and export configuration varaiables
+*/
+
+// Container for all the environments
+let environments = {};
+
+// Staging (default) environment
+
+environments.staging = {
+  'httpPort': 3000,
+  'httpsPort': 3001,
+  'envName': 'staging',
+  'hashingSecret': 'thisIsASecret',
+  'maxChecks' : 5
+};
+
+environments.production = {
+  'httpPort': 5000,
+  'httpsPort': 5001,
+  'envName': 'production',
+  'hashingSecret': 'thisIsASecret',
+  'maxChecks' : 5
+};
+
+// Determine wich enviroment was passed as a command-line argument
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase():'';
+
+// Check thta the current environment is one of the environments above, if not, default to Staging
+var environmnetToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = environmnetToExport;
--- index.js
+++ index.js
@@ -98,5 +98,6 @@ let unifiedServer = function(req,res){
 var router = {
   'ping': handlers.ping,
   'users' : handlers.users,
-  'tokens' : handlers.tokens
+  'tokens' : handlers.tokens,
+  'checks' : handlers.checks
 };
